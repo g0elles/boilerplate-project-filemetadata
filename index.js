8e0d0d84f4c4b1e94ab0a0e5780b70a6c2a83eee
Gabri Elles <43645681+g0elles@users.noreply.github.com>
2022-05-30 21:43:22 -0500

    done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const bodyParser = require('body-parser');
 require('dotenv').config()
+
 
 var app = express();
 
@@ -8,10 +11,19 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
+app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
+  console.log(req.file);
+  let result = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  }
+  res.json(result);
+});
 
 
 const port = process.env.PORT || 3000;
